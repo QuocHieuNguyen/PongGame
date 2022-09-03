9e8cba6b7d55117839cccccbe1241b51bf85e053
Nguyen Quoc Hieu <quochieuforwork@gmail.com>
2022-09-03 22:22:38 +0700

    add package and test successfully

--- PongGame/Assets/Game/Scripts/Utils/SocketIOTest/NetworkClient.cs
+++ PongGame/Assets/Game/Scripts/Utils/SocketIOTest/NetworkClient.cs
@@ -1,13 +1,13 @@
 using System.Collections;
 using System.Collections.Generic;
-using Socket.Quobject.SocketIoClientDotNet.Client;
+
 using UnityEngine;
 public class NetworkClient : MonoBehaviour {
-    private QSocket socket;
+
 
     void Start () {
         Debug.Log ("start");
-        socket = IO.Socket ("http://localhost:3000");
+        /*socket = IO.Socket ("http://localhost:3000");
 
         socket.On (QSocket.EVENT_CONNECT, () => {
             Debug.Log ("Connected");
@@ -16,10 +16,10 @@ public class NetworkClient : MonoBehaviour {
 
         socket.On ("chat message", data => {
             Debug.Log ("data : " + data);
-        });
+        });*/
     }
     private void OnDestroy () {
-        socket.Disconnect ();
+        /*socket.Disconnect ();*/
     }
     // Update is called once per frame
     void Update () {
