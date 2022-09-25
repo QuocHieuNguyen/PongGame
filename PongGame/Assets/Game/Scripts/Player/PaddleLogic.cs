c615f11473bc170e17414cd411ee587c2daf4d34
Nguyen Quoc Hieu <quochieuforwork@gmail.com>
2022-09-25 20:01:05 +0700

    refactor paddle prefab

--- PongGame/Assets/Game/Scripts/Player/PaddleLogic.cs
+++ PongGame/Assets/Game/Scripts/Player/PaddleLogic.cs
@@ -16,4 +16,14 @@ public class PaddleLogic
         this.paddleInput = paddleInput;
         this.paddleSimulation = paddleSimulation;
     }
+
+    public void Update(float deltaTime)
+    {
+        float inputAxisReading = paddleInput.ReadInput();
+        float yPos = paddleSimulation.UpdatePosition(inputAxisReading, paddleData.MovementSpeedScaleFactor, deltaTime);
+
+        Vector3 curPos = positionAdapter.Position;
+        Vector3 newPos = new Vector3(curPos.x, yPos * paddleData.PositionScale, curPos.z);
+        positionAdapter.Position = newPos;
+    }
 }
--- PongGame/Assets/Game/Scripts/Player/PaddleMonoBehavior.cs
+++ PongGame/Assets/Game/Scripts/Player/PaddleMonoBehavior.cs
@@ -4,19 +4,23 @@ using UnityEngine;
 
 public class PaddleMonoBehavior : MonoBehaviour, IPositionAdapter
 {
+    public PaddleData paddleData;
+    [SerializeField] private string inputAxisName;
     private PaddleInput paddleInput;
     private PaddleSimulation paddleSimulation;
     private PaddleLogic paddleLogic;
     // Start is called before the first frame update
-    void Start()
+    void Awake()
     {
-        
+        paddleInput = new PaddleInput(inputAxisName);
+        paddleSimulation = new PaddleSimulation();
+        paddleLogic = new PaddleLogic(this, paddleData, paddleInput, paddleSimulation);
     }
 
     // Update is called once per frame
-    void Update()
+    void FixedUpdate()
     {
-        
+        paddleLogic.Update(Time.deltaTime);
     }
 
     public Vector3 Position
